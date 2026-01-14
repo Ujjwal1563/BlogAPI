e33062b8b7400de2d88319e437b35b585966aa3c
Ujjwal Sharma <ujjwal.shar.ma1563@gmail.com>
2026-01-14 20:44:23 +0530

    Register - User Schema Setup

---
+++ src/controllers/v1/auth/register.ts
@@ -0,0 +1,29 @@
+import {logger} from "@/lib/winston"
+
+import config from '@/config';
+
+import User from '@/models/user';
+
+import type{Request,Response} from 'express';
+import type{IUser} from '@/models/user';
+
+type UserData = Pick<IUser, 'email' | 'password' | 'role'>
+
+const register = async (req:Request, res:Response):Promise<void>=>{
+    const {email, password, role} = req.body as UserData;
+    try {
+        res.status(201).json({
+            message:'New user created'
+        });
+    }
+    catch (err){
+        res.status(500).json({
+            code:'ServerError',
+            message:'Internal server error',
+            error:err
+        });
+        logger.error('Error during user registration')
+    }
+}
+
+export default register;
